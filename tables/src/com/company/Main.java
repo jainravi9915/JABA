a5d6887fb2391eca78aac6787eeab061685ad2c4
vladvodopad26@gmail.com <2upo>
2020-09-22 18:53:30 +0300

    fixes

--- tables/src/com/company/Main.java
+++ tables/src/com/company/Main.java
@@ -1,6 +1,4 @@
 package com.company;
-import java.lang.Math;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.awt.datatransfer.StringSelection;
 import java.awt.Toolkit;
@@ -10,8 +8,9 @@ import static java.lang.System.out;
 
 public class Main {
 
-    public static void tableInit(String[][] table, String text){
+    public static void tableInit(String[][] table) {
         Scanner scan = new Scanner(System.in);
+        String text;
 
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 4; j++){
@@ -26,21 +25,21 @@ public class Main {
         }
     }
     public static String tableAlignment(String[][] table){
-        int maxLenghtString = 0;
-        String s = new String("");
+        int maxLengthString;
+        StringBuilder s = new StringBuilder();
         for (int i = 0; i < 3; i++){
-            maxLenghtString = 0;
+            maxLengthString = 0;
             for (int j = 0; j < 4; j++) {
-                if (maxLenghtString < table[j][i].length()) {
-                    maxLenghtString = table[j][i].length();
+                if (maxLengthString < table[j][i].length()) {
+                    maxLengthString = table[j][i].length();
                 }
             }
             for (int z = 0; z < 4; z++) {
                 if (z == 1){
-                    table[z][i] += new String(new char[maxLenghtString - table[z][i].length()]).replace("\0", "-");
+                    table[z][i] += new String(new char[maxLengthString - table[z][i].length()]).replace("\0", "-");
                 }
-                else if (table[z][i].length() - maxLenghtString != 0) {
-                    table[z][i] += new String(new char[maxLenghtString - table[z][i].length()]).replace("\0", " ");
+                else if (table[z][i].length() - maxLengthString != 0) {
+                    table[z][i] += new String(new char[maxLengthString - table[z][i].length()]).replace("\0", " ");
                 }
             }
         }
@@ -51,22 +50,27 @@ public class Main {
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 3; j++){
-                s += table[i][j];
+                s.append(table[i][j]);
             }
-            s += "\n";
+            s.append("\n");
         }
-        return s;
+        return s.toString();
     }
 
     public static void main(String[] args) {
 
-	String s = new String("");
-	int rows = 4;
-    int columns = 3;
-	String text = new String();
+	String s;
+	int rows;
+    int columns;
+    Scanner scan = new Scanner(System.in);
+
+    out.println("How many rows: ");
+    rows = scan.nextInt();
+    out.println("How many columns: ");
+    columns = scan.nextInt();
     String [][] table = new String[rows][columns];
 
-    tableInit(table, text);
+    tableInit(table);
     s = tableAlignment(table);
     out.println(s);
 
