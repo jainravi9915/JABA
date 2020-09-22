8c8586c5a65981be385e4358fda13f26686f8f60
vladvodopad26@gmail.com <2upo>
2020-09-22 18:37:38 +0300

    add more func

--- tables/src/com/company/Main.java
+++ tables/src/com/company/Main.java
@@ -10,7 +10,9 @@ import static java.lang.System.out;
 
 public class Main {
 
-    public static void tableInit(String[][] table, Scanner scan, String text){
+    public static void tableInit(String[][] table, String text){
+        Scanner scan = new Scanner(System.in);
+
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 4; j++){
                 if (j == 1){
@@ -23,53 +25,54 @@ public class Main {
             }
         }
     }
+    public static String tableAlignment(String[][] table){
+        int maxLenghtString = 0;
+        String s = new String("");
+        for (int i = 0; i < 3; i++){
+            maxLenghtString = 0;
+            for (int j = 0; j < 4; j++) {
+                if (maxLenghtString < table[j][i].length()) {
+                    maxLenghtString = table[j][i].length();
+                }
+            }
+            for (int z = 0; z < 4; z++) {
+                if (z == 1){
+                    table[z][i] += new String(new char[maxLenghtString - table[z][i].length()]).replace("\0", "-");
+                }
+                else if (table[z][i].length() - maxLenghtString != 0) {
+                    table[z][i] += new String(new char[maxLenghtString - table[z][i].length()]).replace("\0", " ");
+                }
+            }
+        }
+
+        for (int i = 0; i < 4; i++){
+            table[i][2] += " |";
+        }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++){
+                s += table[i][j];
+            }
+            s += "\n";
+        }
+        return s;
+    }
 
     public static void main(String[] args) {
 
-	Scanner scan = new Scanner(System.in);
-	int maxLenghtString = 0;
 	String s = new String("");
 	int rows = 4;
     int columns = 3;
 	String text = new String();
-
-
     String [][] table = new String[rows][columns];
-    tableInit(table, scan, text);
 
-    for (int i = 0; i < 3; i++){
-        maxLenghtString = 0;
-        for (int j = 0; j < 4; j++) {
-            if (maxLenghtString < table[j][i].length()) {
-                maxLenghtString = table[j][i].length();
-            }
-        }
-        for (int z = 0; z < 4; z++) {
-            if (z == 1){
-                table[z][i] += new String(new char[maxLenghtString - table[z][i].length()]).replace("\0", "-");
-            }
-            else if (table[z][i].length() - maxLenghtString != 0) {
-                table[z][i] += new String(new char[maxLenghtString - table[z][i].length()]).replace("\0", " ");
-            }
-        }
-    }
+    tableInit(table, text);
+    s = tableAlignment(table);
+    out.println(s);
 
-    for (int i = 0; i < 4; i++){
-            table[i][2] += " |";
-    }
-
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 3; j++){
-            s += table[i][j];
-        }
-        s += "\n";
-    }
-
-    out.print(s);
     StringSelection stringSelection = new StringSelection(s);
     Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
     clipboard.setContents(stringSelection, null);
-
 
     }
 }
